cb27d3dd7e8de0eed016ad471392cdfcf5b9fc9b
Guillermo Gamboa <gamboaguillermo12@gmail.com>
2022-05-23 20:41:48 -0600

    Ya aparece la comida aleatoriamente

--- JUEGO_CULEBRITA.py
+++ JUEGO_CULEBRITA.py
@@ -1,4 +1,5 @@
 #Definimos las diferentes librerías que utilizaremos. 
+from re import purge
 import pygame
 import random
 
@@ -6,7 +7,7 @@ pygame.init()
 pygame.font.init()
 
 #Clase Principal
-class DISEÑO():
+class DISEÑO(pygame.sprite.Sprite):
     def __init__(self):
         pass
 
@@ -20,13 +21,10 @@ class DISEÑO():
 
         #-------------------IMÁGENES
         self.fondo=pygame.image.load("fondo.png")
+        self.comida=pygame.image.load("comida.png").convert()
+        self.comida.set_colorkey(self.blanco)
+        self.rect=self.comida.get_rect()
         #self.icono=pygame.image.load("icono.png")
-
-        #-------------------PANTALLA
-        self.tamaño_pantalla=[900,650]
-        self.pantalla=pygame.display.set_mode(self.tamaño_pantalla)
-        pygame.display.set_caption("CULEBRITA")
-        #pygame.display.set_icon(self.icono) ---> AÚN FALTA EL ÍCONO
 
         #-------------------FUENTES DE TEXTO
         self.fuente_numeros=pygame.font.SysFont("Cooper",40)
@@ -35,6 +33,16 @@ class DISEÑO():
         
         #-------------------VARIABLES DE APOYO
         self.inicio=True
+        self.cuadritos=50
+        self.x=random.randrange(self.cuadritos,self.tamaño_pantalla[0]-self.cuadritos,self.cuadritos)
+        self.y=random.randrange(self.cuadritos*2,self.tamaño_pantalla[1]-self.cuadritos,self.cuadritos)
+
+    def pantalla_de_juego(self):
+
+        self.tamaño_pantalla=[900,650]
+        self.pantalla=pygame.display.set_mode(self.tamaño_pantalla)
+        pygame.display.set_caption("CULEBRITA")
+        #pygame.display.set_icon(self.icono) ---> AÚN FALTA EL ÍCONO
 
     def marco(self):
         grosor = 8
@@ -45,10 +53,17 @@ class DISEÑO():
 
         # Líneas verticales del área de juego.
         pygame.draw.line(self.pantalla,self.negro,(50,97),(50,604), grosor)
-        pygame.draw.line(self.pantalla,self.negro,(850,97),(850,604), grosor)
-
+        pygame.draw.line(self.pantalla,self.negro,(850,97),(850,604), grosor)       
         
-class JUEGO_FINAL(DISEÑO):
+class LOGICA(DISEÑO):
+    def __init__(self):
+        super().__init__()
+
+    def comida_aleatoria(self):
+        self.x=random.randrange(self.cuadritos,self.tamaño_pantalla[0]-self.cuadritos,self.cuadritos)
+        self.y=random.randrange(self.cuadritos*2,self.tamaño_pantalla[1]-self.cuadritos,self.cuadritos)
+
+class JUEGO_FINAL(LOGICA):
     def __init__(self):
         super().__init__()
 
@@ -56,18 +71,32 @@ class JUEGO_FINAL(DISEÑO):
         for evento in pygame.event.get():
             if evento.type==pygame.QUIT:
                 self.inicio=False
+            
+            # TECLAS
+            if evento.type==pygame.KEYDOWN:
+                #SOLAMENTE EJEMPLO DE COMIDA ALEATORIA
+                if evento.key==pygame.K_DOWN:
+                    self.comida_aleatoria()
         self.marco()
-        
 
     def juego_culebrita(self):
+        self.pantalla_de_juego()
         self.variables()
         while self.inicio:
             self.pantalla.blit(self.fondo,(0,0))
-            #self.pantalla.fill(self.blanco)
-            self.eventos_general()
 
+            self.eventos_general()
+            self.pantalla.blit(self.comida,(self.x,self.y))
+            
             #Actualizar pantalla
             pygame.display.flip()
 
 pruebas=JUEGO_FINAL()
 pruebas.juego_culebrita()
+
+
+#-------------------COMENTARIOS------------------
+# Quitar la comida con la tecla de abajo, solo fue ejemplo
+# Pensar como colocar a la culebrita. 
+# Diseñar ícono para la pantalla de juego
+# Pensar en el menú.
