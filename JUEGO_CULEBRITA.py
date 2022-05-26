c4c8616c68d75beca550174d8eee296a0cfb840b
Guillermo Gamboa <gamboaguillermo12@gmail.com>
2022-05-25 21:10:28 -0600

    Ya se mueve la cabeza de la serpiente

--- JUEGO_CULEBRITA.py
+++ JUEGO_CULEBRITA.py
@@ -2,13 +2,11 @@
 import pygame
 import random
 
-from zmq import EVENT_ACCEPT_FAILED
-
 pygame.init()
 pygame.font.init()
 
 #Clase Principal
-class DISEÑO(pygame.sprite.Sprite):
+class DISEÑO():
     def __init__(self):
         pass
 
@@ -19,24 +17,15 @@ class DISEÑO(pygame.sprite.Sprite):
         self.rojo_claro=(207,100,79)
         self.blanco=(255,255,255)
         self.negro=(0,0,0)
-        self.verde=(95,230,49)
+        self.verde=(95,230,49)       
 
         #-------------------IMÁGENES
         self.fondo=pygame.image.load("fondo.png")
-                #---Comida
-        self.comida=pygame.image.load("comida.png").convert()
-        self.comida.set_colorkey(self.blanco)
-        self.rect=self.comida.get_rect()
-                #---Cabeza de la serpiente
-        self.cabeza=pygame.image.load("cabeza.png").convert()
-        self.cabeza.set_colorkey(self.blanco)
-        self.rect=self.cabeza.get_rect()
-
         #self.icono=pygame.image.load("icono.png")
 
         #-------------------FUENTES DE TEXTO
-        self.fuente_numeros=pygame.font.SysFont("Cooper",40)
-        self.fuente_titulos=pygame.font.SysFont("Snap ITC",80)
+        self.fuente_numeros=pygame.font.SysFont("Snap ITC",40)
+        self.fuente_titulos=pygame.font.SysFont("Cooper",80)
         self.fuente_textos=pygame.font.SysFont("Arial",20)
         
         #-------------------VARIABLES DE APOYO
@@ -44,8 +33,12 @@ class DISEÑO(pygame.sprite.Sprite):
         self.cuadritos=50
         self.x=random.randrange(self.cuadritos,self.tamaño_pantalla[0]-self.cuadritos,self.cuadritos)
         self.y=random.randrange(self.cuadritos*2,self.tamaño_pantalla[1]-self.cuadritos,self.cuadritos)
-        self.angulo=90
-        self.direccion="arriba"
+        self.angulo=0
+        self.direccion="derecha"
+        self.coorde_x_culebra=400
+        self.coorde_y_culebra=350
+        self.velocidad_culebra=2
+        self.contador=0
 
     def pantalla_de_juego(self):
 
@@ -65,19 +58,49 @@ class DISEÑO(pygame.sprite.Sprite):
         pygame.draw.line(self.pantalla,self.negro,(50,97),(50,604), grosor)
         pygame.draw.line(self.pantalla,self.negro,(850,97),(850,604), grosor)    
  
-        
-class LOGICA(DISEÑO):
+class SPRITES(DISEÑO,pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        pygame.sprite.Sprite.__init__(self)
+
+    def sprites(self):
+                #---Comida
+        self.comida=pygame.image.load("comida.png").convert()
+        self.comida.set_colorkey(self.blanco)
+        self.rect=self.comida.get_rect()
+
+                #---Cabeza de la serpiente
+        self.cabeza=pygame.image.load("cabeza.png").convert()
+        self.cabeza.set_colorkey(self.blanco)
+        self.rect=self.cabeza.get_rect()
+
+                #---Cuerpo de la serpiente
+        self.cuerpo=pygame.image.load("cuerpo.png").convert()
+        self.cuerpo.set_colorkey(self.blanco)
+        self.rect=self.cuerpo.get_rect()
+
+                #---Cola de la serpiente
+        self.cola=pygame.image.load("cola.png").convert()
+        self.cola.set_colorkey(self.blanco)
+        self.rect=self.cola.get_rect() 
+
+class LOGICA(SPRITES):
     def __init__(self):
         super().__init__()
 
     def comida_aleatoria(self):
+        #---La comida aparece en lugares aleatorios del área de juego
         self.x=random.randrange(self.cuadritos,self.tamaño_pantalla[0]-self.cuadritos,self.cuadritos)
         self.y=random.randrange(self.cuadritos*2,self.tamaño_pantalla[1]-self.cuadritos,self.cuadritos)
 
     def cuerpo_serpiente(self):
-        self.culebra=pygame.draw.rect(self.pantalla,self.rojo_claro,(400,400,30,40),0,15)
+        self.pantalla.blit(self.cabeza,(self.coorde_x_culebra,self.coorde_y_culebra))
+        self.pantalla.blit(self.cuerpo,(self.coorde_x_culebra-self.cuadritos,self.coorde_y_culebra))
+        self.pantalla.blit(self.cola,(self.coorde_x_culebra-self.cuadritos*2,self.coorde_y_culebra))
 
     def cambio_direccion(self):
+        #---Dependiendo del ángulo en el que se encuentre la serpiente así será su moviemiento
+        #---Evitando que hagamos un movimiendo en sentido contrario
         if self.direccion=="abajo":
             if self.angulo==0:
                 self.cabeza=pygame.transform.rotate(self.cabeza,-90)
@@ -106,6 +129,29 @@ class LOGICA(DISEÑO):
             elif self.angulo==90:
                 self.cabeza=pygame.transform.rotate(self.cabeza,-90)
 
+    def avance(self):
+        #---Dependiendo del ángulo al que se dirija así será su desplazamiento
+        if self.angulo==0:
+            self.coorde_x_culebra+=self.velocidad_culebra
+
+        elif self.angulo==180:
+            self.coorde_x_culebra-=self.velocidad_culebra
+
+        elif self.angulo==270:
+            self.coorde_y_culebra+=self.velocidad_culebra
+
+        elif self.angulo==90:
+            self.coorde_y_culebra-=self.velocidad_culebra
+
+            #---Detecta las coordenadas de los bordes del área de juego.        
+        if self.coorde_x_culebra<50 or self.coorde_x_culebra>800:
+            self.inicio=False
+        if self.coorde_y_culebra<100 or self.coorde_y_culebra>550:
+            self.inicio=False
+
+            #PUNTOS - NO FUNCIONA AL 100%
+        if self.coorde_x_culebra==self.x and self.coorde_y_culebra==self.y:
+            self.contador+=1
 
 class JUEGO_FINAL(LOGICA):
     def __init__(self):
@@ -126,6 +172,7 @@ class JUEGO_FINAL(LOGICA):
                         self.angulo=90
                     else:
                         self.angulo=270
+                    #self.coorde_y_culebra+=10
                     
                 if evento.key==pygame.K_UP:
                     self.direccion="arriba"
@@ -134,6 +181,7 @@ class JUEGO_FINAL(LOGICA):
                         self.angulo=270
                     else:
                         self.angulo=90
+                    #self.coorde_y_culebra+=-10
 
                 if evento.key==pygame.K_LEFT:
                     self.direccion="izquierda"
@@ -142,6 +190,7 @@ class JUEGO_FINAL(LOGICA):
                         self.angulo=0
                     else:
                         self.angulo=180
+                    #self.coorde_x_culebra+=-10
 
                 if evento.key==pygame.K_RIGHT:
                     self.direccion="derecha"
@@ -150,19 +199,28 @@ class JUEGO_FINAL(LOGICA):
                         self.angulo=180
                     else:
                         self.angulo=0
+                    #self.coorde_x_culebra+=10
 
         self.marco()
 
     def juego_culebrita(self):
         self.pantalla_de_juego()
         self.variables()
+        self.sprites()
         while self.inicio:
             self.pantalla.blit(self.fondo,(0,0))
             self.cuerpo_serpiente()
-
+    
             self.eventos_general()
             self.pantalla.blit(self.comida,(self.x,self.y))
-            self.pantalla.blit(self.cabeza,(400,350))
+            self.avance()
+            self.cuerpo_serpiente()
+            contador=self.fuente_numeros.render("Puntos:  "+str(self.contador),1,self.negro)
+            self.pantalla.blit(contador,(10,5))
+
+            #----Detectar Colisiones
+            #if 
+
             #Actualizar pantalla
             pygame.display.flip()
 
@@ -171,8 +229,8 @@ pruebas.juego_culebrita()
 
 
 #-------------------COMENTARIOS------------------
-# Quitar la comida con la tecla de abajo, solo fue ejemplo
-# Añadir movimiento a la culebrita
+# Falta que cuente los puntos al momento de tocar la comida
+# Averiguar sobre las colisiones, ya que el método que uso no me deja
 # Pensar como colocar el cuaerpo de la culebrita y que la siga. 
 # Diseñar ícono para la pantalla de juego
 # Pensar en el menú.
